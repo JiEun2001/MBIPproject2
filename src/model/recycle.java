13047627a464a4eee6daeeca7b61e2080e3f0649
Muhd Afnan <88524435+zeynanz@users.noreply.github.com>
2024-02-06 22:15:45 +0800

    getter setter recycle & electric

---
+++ src/model/recycle.java
@@ -0,0 +1,43 @@
+package model;
+
+public class recycle {
+	private int reid;
+	private int	uid;
+	private String reItem;
+	private int reTotalItem;
+	private double reWeight;
+	public int getReid() {
+		return reid;
+	}
+	public void setReid(int reid) {
+		this.reid = reid;
+	}
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getReItem() {
+		return reItem;
+	}
+	public void setReItem(String reItem) {
+		this.reItem = reItem;
+	}
+	public int getReTotalItem() {
+		return reTotalItem;
+	}
+	public void setReTotalItem(int reTotalItem) {
+		this.reTotalItem = reTotalItem;
+	}
+	public double getReWeight() {
+		return reWeight;
+	}
+	public void setReWeight(double reWeight) {
+		this.reWeight = reWeight;
+	}
+	
+	
+	
+}
+	
